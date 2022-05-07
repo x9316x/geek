602859774607b7df5c7d434cfda0cf603da5ac53
x9316x <x9316x@gmail.com>
2022-05-07 19:46:38 +0300

    add array input and output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длинна которых меньше либо равна 3 символам.
+
+
+
+// ввод массива с клавиатуры
+string[] ArrayInput(int originalArrayLength)
+{
+    string[] originalArray = new string[originalArrayLength];
+
+    for(int index = 0; index < originalArrayLength; index++)
+    {
+        Console.WriteLine("Введите " + (index + 1) + " элемент массива");
+        string? arrayElementTempVaraible = Console.ReadLine();
+        
+        while(arrayElementTempVaraible == null)
+        {
+            arrayElementTempVaraible = Console.ReadLine();
+        }
+        
+        originalArray[index] = arrayElementTempVaraible;
+    }
+
+    return originalArray;
+}
+
+// определяем количество элементов в изначальном массивеЫ
+Console.WriteLine("Укажите количество элементов в предполагаемом массиве:");
+int originalArrayLength = Convert.ToInt32(Console.ReadLine());
+
+// вывод изначального массива
+string[] originalArray = ArrayInput(originalArrayLength); 
+
+for(int i = 0; i < originalArrayLength; i++)
+{
+    Console.Write(originalArray[i] + " "); 
+} 
